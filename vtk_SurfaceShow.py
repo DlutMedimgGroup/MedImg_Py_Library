9e682164da3105c85920f24f28a521ab38b92cb3
zengxiao <zengxiao@mail.dlut.edu.cn>
2018-07-20 16:59:05 +0800

    Modified
    Change to a simple display method.

--- vtk_SurfaceShow.py
+++ vtk_SurfaceShow.py
@@ -2,76 +2,42 @@
 # -*- coding: UTF-8 -*-
 """
 Script Name	  : itkVectorFuzzyConnectednessImageFilter
-Author		  : genispan(CSDN)
+Author		  : zengxiao
 Created		  : 2018/7/11
-Version		  : 1.0
+Version		  : 2.0
 Description	  : Sorry, I didn't understand how does the code work, so I just copied genispan's code from CSDN.
                 The courses arrangement of this week is too full, and I have not been able to explore it very well.
   PURPOSE     : Vtk display surface
+  Modified    : Change to a simple display method.
 """
 
 import vtk
 
-aRenderer = vtk.vtkRenderer();
-renWin = vtk.vtkRenderWindow();
-renWin.AddRenderer(aRenderer);
-iren = vtk.vtkRenderWindowInteractor();
-iren.SetRenderWindow(renWin);
+filename = "Liver.stl"
 
-v16 = vtk.vtkDICOMImageReader();
-v16.SetDataByteOrderToLittleEndian();
-v16.SetDirectoryName("D:\PythonCode\zx\Liver.stl");
-v16.SetDataSpacing(3.2, 3.2, 1.5);
+reader = vtk.vtkSTLReader()
+reader.SetFileName(filename)
 
-skinExtractor = vtk.vtkContourFilter();
-skinExtractor.SetInputConnection(v16.GetOutputPort());
-skinExtractor.SetValue(0, 500);
-skinNormals = vtk.vtkPolyDataNormals();
-skinNormals.SetInputConnection(skinExtractor.GetOutputPort());
-skinNormals.SetFeatureAngle(60.0);
-skinMapper = vtk.vtkPolyDataMapper();
-skinMapper.SetInputConnection(skinNormals.GetOutputPort());
-skinMapper.ScalarVisibilityOff();
+mapper = vtk.vtkPolyDataMapper()
 
-skin = vtk.vtkActor();
-skin.SetMapper(skinMapper);
+mapper.SetInputConnection(reader.GetOutputPort())
 
-outlineData = vtk.vtkOutlineFilter();
-outlineData.SetInputConnection(v16.GetOutputPort());
-mapOutline = vtk.vtkPolyDataMapper();
-mapOutline.SetInputConnection(outlineData.GetOutputPort());
-outline = vtk.vtkActor();
-outline.SetMapper(mapOutline);
-outline.GetProperty().SetColor(0, 0, 0);
+actor = vtk.vtkActor()
+actor.SetMapper(mapper)
 
-aCamera = vtk.vtkCamera();
-aCamera.SetViewUp(0, 0, -1);
-aCamera.SetPosition(0, 1, 0);
-aCamera.SetFocalPoint(0, 0, 0);
-aCamera.ComputeViewPlaneNormal();
+# Create a rendering window and renderer
+ren = vtk.vtkRenderer()
+renWin = vtk.vtkRenderWindow()
+renWin.AddRenderer(ren)
 
-aRenderer.AddActor(outline);
-aRenderer.AddActor(skin);
-aRenderer.SetActiveCamera(aCamera);
-aRenderer.ResetCamera();
-aCamera.Dolly(1.5);
+# Create a renderwindowinteractor
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renWin)
 
-aRenderer.SetBackground(1, 1, 1);
-renWin.SetSize(640, 480);
-aRenderer.ResetCameraClippingRange();
+# Assign actor to the renderer
+ren.AddActor(actor)
 
-iren.Initialize();
-iren.Start();
-
-v16.Delete();
-skinExtractor.Delete();
-skinNormals.Delete();
-skinMapper.Delete();
-skin.Delete();
-outlineData.Delete();
-mapOutline.Delete();
-outline.Delete();
-aCamera.Delete();
-iren.Delete();
-renWin.Delete();
-aRenderer.Delete();
+# Enable user interface interactor
+iren.Initialize()
+renWin.Render()
+iren.Start()
